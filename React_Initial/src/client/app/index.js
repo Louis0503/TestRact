d034a2c4489c8139f8bb90b07b1307e59ef9f9a9
LouisLiu <louis19820503@gmail.com>
2016-09-29 17:01:13 +0800

    Test MarkdownEditor

--- React_Initial/src/client/app/index.js
+++ React_Initial/src/client/app/index.js
@@ -6,9 +6,11 @@ import HelloMessage from "./components/HelloMessage";
 import MyComponent from "./components/MyComponent";
 import Timer from "./components/Timer";
 import ToDoApp from "./components/ToDoApp";
-
+import MarkdownEditor from "./components/MarkdownEditor";
+<script src="https://cdn.jsdelivr.net/remarkable/1.6.2/remarkable.min.js"></script>
 // 將 <MyComponent /> 元件插入 id 為 app 的 DOM 元素中
 ReactDOM.render(<HelloMessage/>, document.getElementById('hello'));
 ReactDOM.render(<MyComponent/>, document.getElementById('myComponent'));
 ReactDOM.render(<Timer/>, document.getElementById('timer'));
 ReactDOM.render(<ToDoApp/>, document.getElementById('todoApp'));
+ReactDOM.render(<MarkdownEditor />, document.getElementById('markdownTest'));
